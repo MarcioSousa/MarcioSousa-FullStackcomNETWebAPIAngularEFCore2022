b1cc77c1598666620d291890ba2a5483399c558f
MarcioSousa <marciofrapeso@gmail.com>
2022-05-10 21:05:27 -0300

    3 - EF Core 5 - Introdução

--- 2_DotNet_Core_5/ProEventos/Back/src/ProEventos.API/Controllers/EventoController.cs
+++ 2_DotNet_Core_5/ProEventos/Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,41 +13,23 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public IEnumerable<Evento> _evento = new Evento[]{
-            new Evento(){
-                EventoId = 1,
-                Tema = "Angular 11 e .NET 5",
-                Local = "Belo Horizonte",
-                Lote = "1º Lote",
-                QtdePessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString(),
-                ImagemUrl = "foto.png"
-            },    
-            new Evento(){
-                EventoId = 2,
-                Tema = "Angular 11 e suas novidades",
-                Local = "Belo Horizonte",
-                Lote = "2º Lote",
-                QtdePessoas = 350,
-                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-                ImagemUrl = "foto.png"
-            }
-        };
-        public EventoController(ILogger<EventoController> logger)
+        private readonly DataContext context;
+        public EventoController(DataContext context)
         {
+            this.context = context;
 
         }
 
         [HttpGet]
-        public IEnumerable<Evento> Get(int id)
+        public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
 
         [HttpPost]
@@ -61,7 +44,7 @@ namespace ProEventos.API.Controllers
             return $"Exemplo de Put com id = {id}";
         }
 
-         [HttpDelete("{id}")]
+        [HttpDelete("{id}")]
         public string Delete(int id)
         {
             return $"Exemplo de Delete com id = {id}";
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# FullStackcomNETWebAPIAngularEFCore2022
-1 - Introdução
-2 - DotNet Core 5
+# Full-Stack com DotNET WebAPI Angular EF Core 2022
+1 - Introdução <br>
+2 - DotNet Core 5 <br>
+3 - EF Core 5 - Introdução <br>
+4 - Angular - Introdução <br>
