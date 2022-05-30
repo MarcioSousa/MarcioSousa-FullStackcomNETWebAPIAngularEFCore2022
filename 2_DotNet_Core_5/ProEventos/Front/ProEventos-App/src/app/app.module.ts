473054ea24387767083b77a4d175de8fbc93a5c6
MarcioSousa <marciofrapeso@gmail.com>
2022-05-30 15:23:15 -0300

    88 - NGX Spinner - Carregando.

--- 2_DotNet_Core_5/ProEventos/Front/ProEventos-App/src/app/app.module.ts
+++ 2_DotNet_Core_5/ProEventos/Front/ProEventos-App/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
