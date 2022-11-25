090f95cf8d1bd030de58f378b308cd942848a99d
MarcioSousa <marciofrapeso@gmail.com>
2022-11-24 22:45:54 -0300

    Adicionando referências.

--- 3_MeuProjeto/ProEscola/Back/src/ProEscola.API/Controllers/WeatherForecastController.cs
+++ 3_MeuProjeto/ProEscola/Back/src/ProEscola.API/Controllers/WeatherForecastController.cs
@@ -4,36 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEscola.API.Models;
 
 namespace ProEscola.API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
+        //private static readonly string[] Summaries = new[]
+        //{
+        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        //};
+        //private readonly ILogger<WeatherForecastController> _logger;
+        //public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        //{
+        //    _logger = logger;
+        //}
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public string Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return "teste";
         }
     }
 }
---
+++ 3_MeuProjeto/ProEscola/Back/src/ProEscola.API/Models/Instrutor.cs
@@ -0,0 +1,8 @@
+namespace ProEscola.API.Models
+{
+    public class Instrutor : Pessoa
+    {
+        public string Formacao { get; set;}
+        public string AreaAtuacao { get; set;}
+    }
+}
