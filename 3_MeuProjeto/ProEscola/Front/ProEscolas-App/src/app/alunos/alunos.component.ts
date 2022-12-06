d3e88102733069a81be6003615a5b2bedd1fd60e
MarcioSousa <marciofrapeso@gmail.com>
2022-12-06 20:46:07 -0300

    Bootstrap funcionando e components configurado para retornar dados da base de dados.

--- 3_MeuProjeto/ProEscola/Front/ProEscolas-App/src/app/alunos/alunos.component.ts
+++ 3_MeuProjeto/ProEscola/Front/ProEscolas-App/src/app/alunos/alunos.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlunosComponent implements OnInit {
 
-  constructor() { }
+  public alunos: any;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getAlunos();
+  }
+
+  public getAlunos(): void{
+    this.http.get('https://localhost:5001/api/Alunos').subscribe(
+      response => this.alunos = response,
+      error => console.log(error)
+    )
   }
 
 }
